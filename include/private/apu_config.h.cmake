2dda00a7f6148918917b48cb1763a86481096f80
David Manura <dm.git@math2.org>
2012-04-06 01:34:41 -0400

    cmake: preliminary conversion to cmake from autoconf

---
+++ include/private/apu_config.h.cmake
@@ -0,0 +1,177 @@
+/* originally based on include/private/apu_config.h.in
+ generated from configure.in by autoheader */
+
+/* Define if the system crypt() function is threadsafe */
+#cmakedefine APU_CRYPT_THREADSAFE
+
+/* Define to 1 if modular components are built as DSOs */
+#cmakedefine APU_DSO_BUILD
+
+/* Define to be absolute path to DSO directory */
+#cmakedefine APU_DSO_LIBDIR
+
+/* Define if the inbuf parm to iconv() is const char ** */
+#cmakedefine APU_ICONV_INBUF_CONST
+
+/* Define that OpenSSL uses const buffers */
+#cmakedefine CRYPTO_OPENSSL_CONST_BUFFERS
+
+/* Define if crypt_r has uses CRYPTD */
+#cmakedefine CRYPT_R_CRYPTD
+
+/* Define if crypt_r uses struct crypt_data */
+#cmakedefine CRYPT_R_STRUCT_CRYPT_DATA
+
+/* Define if CODESET is defined in langinfo.h */
+#cmakedefine HAVE_CODESET
+
+/* Define to 1 if you have the `crypt_r' function. */
+#cmakedefine HAVE_CRYPT_R
+
+/* Define to 1 if you have the declaration of `EVP_PKEY_CTX_new', and to 0 if
+   you don't. */
+#cmakedefine HAVE_DECL_EVP_PKEY_CTX_NEW
+
+/* Define if expat.h is available */
+#cmakedefine HAVE_EXPAT_H
+
+/* Define to 1 if you have the <freetds/sybdb.h> header file. */
+#cmakedefine HAVE_FREETDS_SYBDB_H
+
+/* Define to 1 if you have the <iconv.h> header file. */
+#cmakedefine HAVE_ICONV_H
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H
+
+/* Define to 1 if you have the <langinfo.h> header file. */
+#cmakedefine HAVE_LANGINFO_H
+
+/* Define to 1 if you have the <lber.h> header file. */
+#cmakedefine HAVE_LBER_H
+
+/* Defined if ldap.h is present */
+#cmakedefine HAVE_LDAP_H
+
+/* Define to 1 if you have the <ldap_ssl.h> header file. */
+#cmakedefine HAVE_LDAP_SSL_H
+
+/* Define to 1 if you have the <libpq-fe.h> header file. */
+#cmakedefine HAVE_LIBPQ_FE_H
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine HAVE_MEMORY_H
+
+/* Define to 1 if you have the <mysql.h> header file. */
+#cmakedefine HAVE_MYSQL_H
+
+/* Define to 1 if you have the <mysql/mysql.h> header file. */
+#cmakedefine HAVE_MYSQL_MYSQL_H
+
+/* Define to 1 if you have the <mysql/my_global.h> header file. */
+#cmakedefine HAVE_MYSQL_MY_GLOBAL_H
+
+/* Define to 1 if you have the <mysql/my_sys.h> header file. */
+#cmakedefine HAVE_MYSQL_MY_SYS_H
+
+/* Define to 1 if you have the <my_global.h> header file. */
+#cmakedefine HAVE_MY_GLOBAL_H
+
+/* Define to 1 if you have the <my_sys.h> header file. */
+#cmakedefine HAVE_MY_SYS_H
+
+/* Define to 1 if you have the `nl_langinfo' function. */
+#cmakedefine HAVE_NL_LANGINFO
+
+/* Define to 1 if you have the <nss.h> header file. */
+#cmakedefine HAVE_NSS_H
+
+/* Define to 1 if you have the <nss/nss.h> header file. */
+#cmakedefine HAVE_NSS_NSS_H
+
+/* Define to 1 if you have the <nss/pk11pub.h> header file. */
+#cmakedefine HAVE_NSS_PK11PUB_H
+
+/* Define to 1 if you have the <oci.h> header file. */
+#cmakedefine HAVE_OCI_H
+
+/* Define to 1 if you have the <odbc/sql.h> header file. */
+#cmakedefine HAVE_ODBC_SQL_H
+
+/* Define to 1 if you have the <openssl/x509.h> header file. */
+#cmakedefine HAVE_OPENSSL_X509_H
+
+/* Define to 1 if you have the <pk11pub.h> header file. */
+#cmakedefine HAVE_PK11PUB_H
+
+/* Define to 1 if you have the <postgresql/libpq-fe.h> header file. */
+#cmakedefine HAVE_POSTGRESQL_LIBPQ_FE_H
+
+/* Define to 1 if you have the <prerror.h> header file. */
+#cmakedefine HAVE_PRERROR_H
+
+/* Define to 1 if you have the <sqlite3.h> header file. */
+#cmakedefine HAVE_SQLITE3_H
+
+/* Define to 1 if you have the <sqlite.h> header file. */
+#cmakedefine HAVE_SQLITE_H
+
+/* Define to 1 if you have the <sql.h> header file. */
+#cmakedefine HAVE_SQL_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H
+
+/* Define to 1 if you have the <strings.h> header file. */
+#cmakedefine HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H
+
+/* Define to 1 if you have the <sybdb.h> header file. */
+#cmakedefine HAVE_SYBDB_H
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H
+
+/* Define if xmlparse/xmlparse.h is available */
+#cmakedefine HAVE_XMLPARSE_XMLPARSE_H
+
+/* Define if xmltok/xmlparse.h is available */
+#cmakedefine HAVE_XMLTOK_XMLPARSE_H
+
+/* Define if xml/xmlparse.h is available */
+#cmakedefine HAVE_XML_XMLPARSE_H
+
+/* Define if ldap_set_rebind_proc takes three arguments */
+#cmakedefine LDAP_SET_REBIND_PROC_THREE
+
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#cmakedefine PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#cmakedefine PACKAGE_TARNAME
+
+/* Define to the home page for this package. */
+#cmakedefine PACKAGE_URL
+
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION
+
+/* Define to 1 if you have the ANSI C header files. */
+#cmakedefine STDC_HEADERS
